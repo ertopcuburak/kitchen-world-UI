99de3678adb37e35eacac4064d3b36a012e2fc3a
burak.ertopcu <burak.ertopcu@yandex.com>
2022-07-23 04:03:36 +0300

    init

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,16 +1,63 @@
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
+  const [advice, setAdvice] = useState<any[]>([]);
+  useEffect(() => {
+    const url = "https://cb1b-5-27-31-231.eu.ngrok.io/recipes";
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        setAdvice(json);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    fetchData();
+
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+    
+    <SafeAreaView style={styles.container}>
+      <ScrollView style={styles.scrollView}>
+      {advice.map((recipe, index) => (
+                    // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
+                    <View style={styles.recipes}>
+                      <Text style={styles.title}>
+                      {recipe.name}
+                      </Text>
+                      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+
+                      <View style={styles.materials}>
+                        <Text style={styles.subtitle}>
+                          {'Malzemeler'}
+                        </Text>
+                        {recipe.materialList.map((material:any) => (
+                          <Text>
+                          {material.quantity + ' ' + material.materialName}
+                          </Text>
+                        ))}
+                      </View>
+                      
+                      <Text style={styles.subtitle}>
+                          {'Yapılışı'}
+                        </Text>
+                      <Text style={styles.bodyText}>
+                      {recipe.howToMake}
+                      </Text>
+                    </View>
+                ))}
+      </ScrollView>
+    </SafeAreaView>
+   
   );
 }
 
@@ -19,14 +66,36 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingStart:0,
+    paddingEnd:0
+  },
+  scrollView: {
+    backgroundColor: '#72A2CF',
+    marginHorizontal: 0,
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  subtitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
   },
+  bodyText: {
+    fontSize:16
+  },
+  separator: {
+    marginVertical: 10,
+    height: 1,
+    width: '100%',
+  },
+  recipes: {
+    paddingTop: 20,
+    paddingHorizontal:10
+  },
+  materials: {
+    paddingTop:20,
+    paddingBottom:20,
+    paddingHorizontal:20
+  }
 });
